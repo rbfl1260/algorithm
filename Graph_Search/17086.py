13aed3f76020b6ebc01142eb8b8cbd0d3f48d183
rbfl1260 <seven0126@naver.com>
2024-01-15 13:17:52 +0900

    그래프

--- Graph_Search/17086.py
+++ Graph_Search/17086.py
@@ -1,24 +1,16 @@
 #아기상어2
 from collections import deque
 
-queue=[]
-def bfs(x,y):
-    queue=deque()
-    queue.append((x,y))
-    visited[x][y]=1
-
+queue=deque()
+def bfs():
     while queue:
         x,y=queue.popleft()
         for i in range(8):
             nx=x+dx[i]
             ny=y+dy[i]
-            if 0<=nx<n and 0<=ny<m:
-                if visited[nx][ny]==0 and graph[nx][ny]!=1:
-                    visited[nx][ny]=visited[x][y]+1
-                    queue.append((nx,ny))
-                elif visited[nx][ny] > visited[x][y]+1 and graph[nx][ny]!=1:
-                    visited[nx][ny]=visited[x][y]+1
-    print(visited)
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny]==0:
+                graph[nx][ny]=graph[x][y]+1
+                queue.append((nx,ny))
 
 n,m=map(int,input().split())
 graph=[]
@@ -28,9 +20,14 @@ for i in range(n):
 dx=[-1,1,0,0,1,-1,-1,1]
 dy=[0,0,-1,1,1,1,-1,-1]
 
-visited=[[0]*m for i in range(n)]
-
 for i in range(n):
     for j in range(m):
-        if visited[i][j]==0 and graph[i][j]==1:
-            bfs(i,j)
+        if graph[i][j]==1:
+            queue.append((i,j))
+bfs()
+
+res=0
+for i in graph:
+    for j in i:
+        res=max(res,j)
+print(res-1)
