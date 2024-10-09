a354f68c192bca36025f857cc9ec9e247689afc9
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2024-10-09 22:49:58 +0900

    arr.sort()로 풀어도 된다는 설명 추가

--- Sort/11650.py
+++ Sort/11650.py
@@ -6,6 +6,8 @@ for i in range(N):
     arr.append([x,y])
 
 arr.sort(key=lambda pair:(pair[0],pair[1]))
+#이렇게 굳이 안하고 그냥 arr.sort()해도 됨. 왜냐면 기본적으로 첫번째 요소를 기준으로 정렬하고
+#첫번쨰 원소가 같다면 두번째 요소를 기준으로 정렬해주기 때문에.
 
 for x,y in arr:
     print(x, y)
