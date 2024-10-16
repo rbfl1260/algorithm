10da66b42578ad6b66d0eb52413fa6a53f78a938
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2024-10-16 23:21:07 +0900

    제로

---
+++ Stack)etc/10773.py
@@ -0,0 +1,15 @@
+#제로
+
+import sys
+
+k=int(sys.stdin.readline())
+stack=[]
+
+for i in range(k):
+    num=int(sys.stdin.readline())
+    if num==0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
