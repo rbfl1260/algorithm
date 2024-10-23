4b43176b2512258c4b30881653c9ba51e04a06ef
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2024-10-23 23:57:58 +0900

    팩토리얼 2

--- Recursion/27433.py
+++ Recursion/27433.py
@@ -3,14 +3,10 @@
 import sys
 
 def factorial(N):
-    if N<1:
-        return N
+    if N==0:
+        return 1
     else:
-        N=N*factorial(N-1)
+        return N*factorial(N-1)
 
 n=int(sys.stdin.readline())
-
-if n==0:
-    print(1)
-else:
-    factorial(n)
+print(factorial(n))
