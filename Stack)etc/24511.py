d0ee2b0602dce7d281817c2e1b80e742895ef642
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2024-10-27 18:28:02 +0900

    queuestack

---
+++ Stack)etc/24511.py
@@ -0,0 +1,18 @@
+#queuestack
+import sys
+from collections import deque
+
+n=int(sys.stdin.readline()) #자료구조의 개수
+a=list(map(int,sys.stdin.readline().split()))
+b=list(map(int,sys.stdin.readline().split()))
+m=int(sys.stdin.readline())
+c=list(map(int,sys.stdin.readline().split()))
+
+queue=deque()
+
+for i in range(n):
+    if a[i]==0:
+        queue.append(b[i])
+for i in range(m):
+    queue.appendleft(c[i])
+    print(queue.pop(),end= ' ')
