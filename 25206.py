dccff3435413bc58febb137d4a6af1da6758382b
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-01-25 02:05:02 +0900

    너의 평점은

---
+++ 25206.py
@@ -0,0 +1,22 @@
+#너의 평점은
+
+totalLevel=0.0
+totalCredit=0.0
+grade_to_point = {
+    'A+': 4.5, 'A0': 4.0,
+    'B+': 3.5, 'B0': 3.0,
+    'C+': 2.5, 'C0': 2.0,
+    'D+': 1.5, 'D0': 1.0,
+    'F': 0
+}
+
+for _ in range(20):
+    subject, credit, level=input().split()
+    credit=float(credit)
+
+    if level=='P':
+        continue
+    totalCredit+=credit
+    totalLevel+=grade_to_point[level]*credit
+
+print(totalLevel/totalCredit)
