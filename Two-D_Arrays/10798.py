c97b9c5999609c30692c8208a78d7f78c7467f20
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-01-31 13:42:30 +0900

    주석 추가

--- Two-D_Arrays/10798.py
+++ Two-D_Arrays/10798.py
@@ -4,7 +4,7 @@ arr=[list(input()) for _ in range(5)]
 max_length=max(len(row) for row in arr)
 min_length=min(len(row) for row in arr)
 
-
+#가장 긴 줄 길이어 맞춰 공백으로 채우기
 if min_length != max_length:
     for row in arr:
         while len(row)<max_length:
@@ -12,6 +12,7 @@ if min_length != max_length:
 
 res=[]
 
+#세로 읽기(공백 제외)
 for i in range(max_length):
     for j in range(5):
         if arr[j][i]!=" ":
