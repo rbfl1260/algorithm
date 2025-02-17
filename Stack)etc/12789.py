b1acbe526263d1eb60a89eb7a7773a20f27b372e
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-02-17 18:56:34 +0900

    도키도키 간식드리미

--- Stack)etc/12789.py
+++ Stack)etc/12789.py
@@ -6,23 +6,17 @@ from collections import deque
 n=int(sys.stdin.readline())
 nums=list(map(int,sys.stdin.readline().split()))
 
-res=[] #결과 저장
-wait=deque() #대기열 저장
+stack=deque()
+waitingNum=1
 
-# 1부터 n까지 학생번호
-line=deque(range(1,n+1))
-
-for num in nums:
-  while line:
-     if line[0] ==num:
-        res.append(num)
-        line.popleft()
-        break
-     else:
-        wait.append(num)
-        break
-
-if list(wait) == sorted(wait,reverse=True):
-   print('Nice')
+for x in nums:
+    stack.append(x)
+    while stack and stack[-1] ==waitingNum:
+        print(stack)
+        print(stack[-1])
+        stack.pop()
+        waitingNum+=1
+if not stack:
+    print("Nice")
 else:
-   print('Sad')
+    print("Sad")
