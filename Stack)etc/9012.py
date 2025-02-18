ad30cf79f9fd196fecda66a4cf82aeeab73bf1b8
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-02-18 15:11:33 +0900

    괄호

--- Stack)etc/9012.py
+++ Stack)etc/9012.py
@@ -1,2 +1,22 @@
 #괄호
+from collections import deque
+t=int(input())
 
+stack=deque()
+
+for i in range(t):
+    line=list(input().rstrip())
+
+    stack.clear()
+    for x in line:
+        stack.append(x)
+        while len(stack)>1:
+            if (stack[-2]=='(' and stack[-1]==')'):
+                stack.pop()
+                stack.pop()
+            else:
+                break
+    if not stack:
+        print('YES')
+    else:
+        print('NO')
