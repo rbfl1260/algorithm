8fce11611f04c7c0c3647fd04bc0642d18fdc5f7
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-02-19 14:18:31 +0900

    녹색거탑

--- Combination/24723.py
+++ Combination/24723.py
@@ -1,17 +1,5 @@
 #녹색거탑
-from itertools import combinations
 
-# total=0
 n=int(input())
-# if n==1:
-#     print(2)
-# elif n==2:
-#     print(4)
-# else:
-#     for i in range(2,n+1):
-#         total+=i
 
-#     nums=list(range(total))
-#     res=list(combinations(nums,2))
-#     print(len(res))
 print(2**n)
