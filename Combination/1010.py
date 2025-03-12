10ae6298a1e65b7f089dceafac694e106b79292d
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-03-12 16:30:12 +0900

    다리 놓기

--- Combination/1010.py
+++ Combination/1010.py
@@ -3,8 +3,8 @@
 import math
 
 t=int(input())
-
 for _ in range(t):
     n,m=map(int,input().split())
     result=math.comb(m,n)
+    # 오름차순으로 동쪽사이트에서 N개를 고르는 조합이기 때문이므로 다리가 겹치지 않음
     print(result)
