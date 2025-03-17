eee5c27c8a85e6c5ce9d40e781eb4069ae7b2640
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-03-17 21:40:29 +0900

    전역변수 설정

--- Recursion/24060.py
+++ Recursion/24060.py
@@ -1,19 +1,20 @@
 #알고리즘 수업 - 병합 정렬 1
 import sys
 
-def merge_sort(arr,count,k):
+def merge_sort(arr):
     if len(arr)<=1:
-        return arr,count
+        return arr
     mid=(len(arr)+1)//2
     left=arr[:mid]
     right=arr[mid:]
-    LEFT,count=merge_sort(left,count,k)
-    RIGHT,count=merge_sort(right,count,k)
-    return merge(LEFT,RIGHT,count,k)
+    LEFT=merge_sort(left)
+    RIGHT=merge_sort(right)
+    return merge(LEFT,RIGHT)
 
-def merge(left,right,count,k):
+def merge(left,right):
     i,j=0,0
     sorted_list=[]
+    global count,k
     
     while i<len(left) and j<len(right):
         if left[i]<right[j]:
@@ -21,7 +22,7 @@ def merge(left,right,count,k):
             count+=1
             if count==k:
                 print(left[i])
-                return sorted_list, count
+                return sorted_list
 
             i+=1
         else:
@@ -29,27 +30,27 @@ def merge(left,right,count,k):
             count+=1
             if count==k:
                 print(right[j])
-                return sorted_list, count
+                return sorted_list
             j+=1
     while i<len(left):
         sorted_list.append(left[i])
         count+=1
         if count==k:
             print(left[i])
-            return sorted_list, count
+            return sorted_list
         i+=1
     while j<len(right):
         sorted_list.append(right[j])
         count+=1
         if count==k:
             print(right[j])
-            return sorted_list, count
+            return sorted_list
         j+=1
-    return sorted_list,count
+    return sorted_list
 
 n,k=map(int,sys.stdin.readline().split())
 numbers = list(map(int,sys.stdin.readline().split()))
 count=0
-sorted_list,count=merge_sort(numbers,count,k)
+merge_sort(numbers)
 if count<k:
     print(-1)
