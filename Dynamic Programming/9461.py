bae1a04232eaf6b97bf0f0ca25e3c04572b6475e
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-06-30 23:13:55 +0900

    파도반 수열

---
+++ Dynamic Programming/9461.py
@@ -0,0 +1,13 @@
+MAX=100
+
+dp=[0]*(MAX+1)
+dp[1]=dp[2]=dp[3]=1
+dp[4]=2
+
+for i in range(5,MAX+1):
+    dp[i]=dp[i-1]+dp[i-5]
+
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    print(dp[n])
