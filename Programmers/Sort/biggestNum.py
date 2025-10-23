7f094c673a64feb7bcffee53c7472e2cfd9514d2
최정규 <choejeong-gyu@choejeong-gyuui-MacBookPro.local>
2025-10-23 17:36:49 +0900

    정렬

--- Programmers/Sort/biggestNum.py
+++ Programmers/Sort/biggestNum.py
@@ -1,3 +1,20 @@
+from functools import cmp_to_key
+
 def solution(numbers):
-    answer = ''
-    return answer
+    num_strings=list(map(str,numbers))
+    
+    def compare(a,b):
+        if a+b>b+a:
+            return -1
+        elif a+b < b+a:
+            return 1
+        else:
+            return 0
+    
+    num_strings.sort(key=cmp_to_key(compare))
+
+    answer=''.join(num_strings)
+    return '0' if answer[0]=='0' else answer
+
+num=[6,10,2]
+solution(num)
